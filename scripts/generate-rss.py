28610f7d507585cdf4ebcd1904e996e2899856b9
Jonty Wareing <jonty@jonty.co.uk>
2026-01-27 22:52:14 +0000

    Add description and city/country to RSS feed

--- scripts/generate-rss.py
+++ scripts/generate-rss.py
@@ -4,6 +4,8 @@ from datetime import datetime, timezone
 
 from dateutil import parser
 from feedgen.feed import FeedGenerator
+import reverse_geocode
+
 
 with open('./data/map.json') as f:
     data = json.load(f)
@@ -12,7 +14,7 @@ places_list = sorted(data["places"].values(), key=lambda x: x["created"], revers
 
 fg = FeedGenerator()
 fg.id('https://infrastructureclub.org/travel_guide/')
-fg.title('Infrastructure Club Travel Guide - New Places')
+fg.title('Infrastructure Club Travel Guide')
 fg.author( {'name':'Infrastructure Club'} )
 fg.subtitle('Latest places added to the travel guide')
 fg.link( href='https://github.com/infrastructureclub/travel_guide', rel='alternate' )
@@ -26,11 +28,16 @@ for place in places_list[:50]:
     if not latest_date or date > latest_date:
         latest_date = date
 
+    place["coordinates"].reverse()
+    loc = reverse_geocode.get(place["coordinates"])
+
     fe = fg.add_entry()
     fe.id(place["id"])
     fe.published(published=date)
     fe.updated(updated=date)
-    fe.title(f'{place["name"]} added to the travel guide')
+    fe.title(f'Added "{place["name"]}" in {loc['city']}, {loc['country']}')
+    if "description" in place:
+        fe.description(place["description"])
     fe.link(href=f'https://infrastructureclub.org/travel_guide/#{place["id"]}')
 
 fg.updated(latest_date)
